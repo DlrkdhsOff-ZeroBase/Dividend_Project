9fc64860053bbabeabc5256e05376fb7b81fb00c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-06-27 04:51:30 +0900

    create CompanyRepository, DividendRepository, CompanyEntity, DividendEntity

--- src/main/java/zero/dividend/persist/entity/CompanyEntity.java
+++ src/main/java/zero/dividend/persist/entity/CompanyEntity.java
@@ -1,4 +1,21 @@
 package zero.dividend.persist.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "COMPANY")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String ticker;
+
+    private String name;
 }
