6a7def20c188c7178f41b486b3ea7ee6b5a19ba8
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-01 11:04:37 +0900

    AuthController 구현

--- src/main/java/zero/dividend/web/AuthController.java
+++ src/main/java/zero/dividend/web/AuthController.java
@@ -1,4 +1,38 @@
 package zero.dividend.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zero.dividend.model.Auth;
+import zero.dividend.model.MemberEntity;
+import zero.dividend.security.TokenProvider;
+import zero.dividend.service.MemberService;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUp(@RequestBody Auth.SignUp request) {
+        MemberEntity result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> signIn(@RequestBody Auth.SignIn request) {
+        MemberEntity member = this.memberService.authenticate(request);
+
+        String token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+
+        return ResponseEntity.ok(token);
+    }
 }
