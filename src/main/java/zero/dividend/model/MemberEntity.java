69430b607e079e4424dbae9887aeef7fc391bc32
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-02 12:44:59 +0900

    최종 수정

--- src/main/java/zero/dividend/model/MemberEntity.java
+++ src/main/java/zero/dividend/model/MemberEntity.java
@@ -1,7 +1,5 @@
 package zero.dividend.model;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
